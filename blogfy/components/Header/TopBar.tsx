9fe00f3eb7f97b00f9768efe7f909ebce7dcdcfe
Erik Araujo <erik.doctype@gmail.com>
2026-02-01 12:45:09 -0300

    global css update

--- blogfy/components/Header/TopBar.tsx
+++ blogfy/components/Header/TopBar.tsx
@@ -1,6 +1,6 @@
 export default function TopBar() {
     return (
-        <div className="bg-[#1C1C1C] text-white py-2">
+        <div className="bg-easy-black text-white py-2">
             <div className="max-w-7xl mx-auto px-4 md:px-6 flex flex-col md:flex-row items-center justify-between gap-4">
                 <div className="flex flex-col md:flex-row items-center gap-2 md:gap-4 text-center md:text-left">
                     <h3 className="font-bold text-sm uppercase tracking-wider">Campanha Indique e Ganhe</h3>
